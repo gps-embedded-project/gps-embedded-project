2d2bd72f7d94f757ca45cd3238906d306688fc3e
Ahmed Abdul Rahim <52361929+ahmedahmed3000@users.noreply.github.com>
2021-06-13 23:31:02 +0200

    Add files via upload

--- finaaaaaaaaaaaaal.c
+++ finaaaaaaaaaaaaal.c
@@ -4,10 +4,13 @@
 #include "string.h"
 #include "math.h"
 #include "time.h"
+#include "Stdlib.h"
+
 #define PI 3.141592653589793238462643383279502884L
+//#include "UART.h"
 
-
-
+  
+	
 
 void SystemInit()
 {
@@ -25,6 +28,19 @@ void PortF_Init(void)
 	void PortF_Output(uint32_t data){
 		GPIO_PORTF_DATA_R = data;
 	}
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
@@ -47,6 +63,124 @@ double distance(double x1,double y1,double x2,double y2){
 	
 	return r;	
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Assume a 16 MHz bus clock , creates 9600 baud rate
+void UART_Init(void) {
+  SYSCTL_RCGCUART_R |= 0x0020; // activate UART5
+  SYSCTL_RCGCGPIO_R |= 0x0010; // activate Port E
+  UART5_CTL_R &= ~0x0001; // disable UART
+  UART5_IBRD_R = 104;
+// IBRD = int(16000000/(16*9600)) = int(104.0.1666) = 104
+  UART5_FBRD_R = 11;
+// FBRD = round(0.1666 * 64 + 0.5) = 11.1624 = 11
+  UART5_LCRH_R = 0x0070; // 8-bit word length, enable FIFO 001110000 
+  UART5_CTL_R = 0x0301; // enable RXE, TXE and UART 001100000001
+  GPIO_PORTE_AFSEL_R |= 0x30; // enable alt function on PE4 ,PE5
+  GPIO_PORTE_PCTL_R = (GPIO_PORTE_PCTL_R&0xFF00FFFF)+0x00110000;
+  GPIO_PORTE_DEN_R |= 0x30; // enable digital I/O on PE4, PE5 
+  GPIO_PORTE_AMSEL_R &= ~0x30; // disable analog function on PE4, PE5
+//E4 RX
+//E5 TX
+
+
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	unsigned char UART5_In_Char(void){
+	while((UART5_FR_R&0x10) != 0){}
+	return UART5_DR_R&0xFF;
+}
+
+void UART5_In_String(char *str){
+	char c = UART5_In_Char();
+	while(c && c != ','){
+		*str = c;
+		str++;
+		c = UART5_In_Char();
+	}
+	*str = 0x00;
+}
+
+//$GPRMC,time,A,latitude,N,longitude,E,speed,angle,Date,Magnetic Variation,checksum data
+//$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A
+void gps_read(double * logitude,double * latitude){ 
+	char holder[50];
+	char c;
+	c = UART5_In_Char();
+	if(c == '$'){
+		c = UART5_In_Char();
+		if(c == 'G'){
+			c = UART5_In_Char();
+			if(c == 'P'){
+				c = UART5_In_Char();
+				if(c == 'R'){
+					c = UART5_In_Char();
+					if(c == 'M'){
+						c = UART5_In_Char();
+						if(c == 'C'){
+							c = UART5_In_Char();
+							if(c == ','){
+								c = UART5_In_Char();
+								if(c != ','){ //if there is good data
+									while(c != ','){c = UART5_In_Char();} //revise the following code carefully
+									c = UART5_In_Char();
+									if((c == 'A') || (c == 'V')){
+										c = UART5_In_Char();
+										if(c == ','){
+											UART5_In_String(holder); //Get latitude in holder
+											
+											*latitude = atof(holder);
+											
+											c = UART5_In_Char();//Read N
+											c = UART5_In_Char();//Read ,
+											
+											UART5_In_String(holder);
+											*logitude = atof(holder);
+											return;
+	}}}}}}}}}}
+	*latitude = 0;
+	*logitude = 0;
+	
+}
+	
+
+	
+	
+	
+	
+	
+	
+	
+
+
+
+	
+	
 	
 	
 	
@@ -162,7 +296,91 @@ switch(r)
 int main(){
 	int c=0;
 	int s;
+	double x1=0,y1=0,x2=0,y2=0;
+	PortF_Init();
+	PortA_B_Init();
+  UART_Init();
+	
+	while (x1==0&&y1==0){ 
+	 gps_read(&x1,&y1);
+	}
+		GPIO_PORTF_DATA_R |= 0x08; 
+	
+	
+	while(1) {
+	
+	if ((GPIO_PORTF_DATA_R&0x10)==0x00){
+	     if(c>100)
+{
+GPIO_PORTF_DATA_R &=~ 0x04;
+	GPIO_PORTF_DATA_R |= 0x02;//red
+}
+  gps_read(&x1,&y1);
+			GPIO_PORTF_DATA_R &=~ 0x08;
+	GPIO_PORTF_DATA_R |= 0x04; //blue
+  delay(1000);
+	
+		while(1){
 
+	if ((GPIO_PORTF_DATA_R&0x01)==0x01){
+		gps_read(&x2,&y2);
+	c=c+distance(x1,y1,x2,y2); 
+  x1=x2;
+  y1=y2;
+  delay(1000);
+		
+	GPIO_PORTF_DATA_R|=0x02;
+	}
+	
+	
+	
+	
+ 	else if ((GPIO_PORTF_DATA_R&0x01)==0x00){
+
+  	
+	while(1){     //we will replace 1 be push button condition
+	GPIO_PORTA_DATA_R &=0xDF;
+		GPIO_PORTA_DATA_R |=0xC0 ;
+
+
+	
+			GPIO_PORTB_DATA_R=digit(&c);
+		
+delay(200);
+				
+
+	
+	
+		
+  	GPIO_PORTA_DATA_R &=0xBF;
+	  GPIO_PORTA_DATA_R |=0xA0 ;
+	   GPIO_PORTB_DATA_R=digit(&c);		
+	
+delay(200);
+		
+		GPIO_PORTA_DATA_R &=0x7F;
+		GPIO_PORTA_DATA_R |=0x60;
+	   GPIO_PORTB_DATA_R=digit(&c);		
+		
+delay(200);
+
+
+
+c=s;
+
+}	
+		
+ 
+}
+	
+}
+}
+		
+}
+	
+	
+	/*
+gps_read(&x1,&y1);
 	PortF_Init();
 	c=c+distance(31.244223,30.045307,31.802575,30.051153); // this will give 745
 	s=c;
@@ -197,7 +415,6 @@ delay(200);
 				
 
 	
-	
 		
   	GPIO_PORTA_DATA_R &=0xBF;
 	  GPIO_PORTA_DATA_R |=0xA0 ;
@@ -213,23 +430,13 @@ delay(200);
 
 c=s;
 
-
-
 }
 	
+	*/
+	
+	
+	
 
-
-			
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 return 0;
 }
